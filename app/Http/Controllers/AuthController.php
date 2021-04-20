e8acacb659912109c3f8133e3bc4877e7ff7c4d9
Andri <ajosuesh@gmail.com>
2021-04-20 10:18:11 -0500

    creacion de api y controlador principal

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+
+    ///////////////// LOGIN - SIGNUP - LOGOUT ////////////////
+
+    public function login(Request $request){
+
+    }
+
+    public function signUp(Request $request){
+        
+    }
+
+    public function recuperar(Request $request){
+        
+    }
+
+    public function logout(Request $request){
+        
+    }
+
+    /////////////// FLUJO USER ////////////
+
+    public function user(Request $request){
+        
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,17 +3,24 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::group([
+    'prefix' => 'auth'
+], function () {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('signup', 'AuthController@signUp');
+    Route::post('recuperar', 'AuthController@recuperar');
+
+    Route::group([
+      'middleware' => 'auth:api'
+    ], function() {
+        Route::get('logout', 'AuthController@logout');
+        Route::get('user', 'AuthController@user');
+    });
+
 });
